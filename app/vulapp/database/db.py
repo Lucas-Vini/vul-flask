9262b48daecf66579cfaab3e6883d60f65896224
lucas <lvinicius31@gmail.com>
2024-05-09 13:54:05 -0300

    removing unnecessary db configs

--- app/vulapp/database/db.py
+++ app/vulapp/database/db.py
@@ -14,12 +14,6 @@ def init_db(app):
 	connection_string = f"{config.mysql_user}:{config.mysql_password}@{config.mysql_endpoint}/{config.mysql_database}?client_flag={MULTI_STATEMENTS}"
 
 	app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{}'.format(connection_string)
-	app.config["SQLALCHEMY_ECHO"] = True
-	app.config["SQLALCHEMY_RECORD_QUERIES"] = True
-	app.config['SQLALCHEMY_POOL_SIZE'] = 100
-	app.config['SQLALCHEMY_POOL_RECYCLE'] = 30
-	app.config['SQLALCHEMY_POOL_TIMEOUT'] = 30
-	app.config['SQLALCHEMY_POOL_PRE_PING'] = True
 	app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {"isolation_level": "READ UNCOMMITTED"}
 
 	db.init_app(app)
