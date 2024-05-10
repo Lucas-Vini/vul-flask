1ff1bfe0a0fd25ed137ce94ffbad5e3091268657
lucas <lvinicius31@gmail.com>
2024-05-10 19:57:27 -0300

    removing one more unnecessary db config

--- app/vulapp/database/db.py
+++ app/vulapp/database/db.py
@@ -14,7 +14,6 @@ def init_db(app):
 	connection_string = f"{config.mysql_user}:{config.mysql_password}@{config.mysql_endpoint}/{config.mysql_database}?client_flag={MULTI_STATEMENTS}"
 
 	app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{}'.format(connection_string)
-	app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {"isolation_level": "READ UNCOMMITTED"}
 
 	db.init_app(app)
 
